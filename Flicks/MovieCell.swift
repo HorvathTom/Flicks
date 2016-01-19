0c7307fdae6ed309ebf6075d393cc7f24a57a707
Tom Horvath <thorvath@mail.ccsf.edu>
2016-01-18 23:56:13 -0800

    Implemented quickstart #1-#3

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Tom H on 1/18/16.
+//  Copyright © 2016 Tom Horvath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
